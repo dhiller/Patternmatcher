6c9d59f4fd03b8b301b6f33ea1aa287dc5cb0797
Daniel Hiller <daniel.hiller.1972@googlemail.com>
2011-12-19 18:32:00 +0100

    Further tweaks on layout

--- src/main/java/de/dhiller/patternmatcher/PatternMatcher.java
+++ src/main/java/de/dhiller/patternmatcher/PatternMatcher.java
@@ -31,7 +31,6 @@
 
 package de.dhiller.patternmatcher;
 
-
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
@@ -51,7 +50,8 @@ import javax.swing.text.JTextComponent;
 
 public class PatternMatcher extends JFrame {
 
-    private static final Preferences prefs = Preferences.userNodeForPackage(PatternMatcher.class);
+    private static final Preferences prefs = Preferences
+	    .userNodeForPackage(PatternMatcher.class);
     private final JTextField pattern = new JTextField();
     private final List<JTextArea> testStrings = new ArrayList<JTextArea>();
     private final JTextArea result = new JTextArea();
@@ -61,6 +61,7 @@ public class PatternMatcher extends JFrame {
     private final JPanel textAreaContainer = new JPanel();
     private final JPanel upper = new JPanel();
     private final JPanel upperButtons = new JPanel();
+    private final JLabel labelTestString = new JLabel("Teststring");
 
     private final class TextFieldSizeAdapter implements DocumentListener {
 	private final JTextArea testString;
@@ -76,17 +77,17 @@ public class PatternMatcher extends JFrame {
 
 	@Override
 	public void insertUpdate(DocumentEvent e) {
-		    updateRows(testString);
+	    updateRows(testString);
 	}
 
 	@Override
 	public void changedUpdate(DocumentEvent e) {
-		    updateRows(testString);
+	    updateRows(testString);
 	}
 
 	private void updateRows(JTextArea testString) {
 	    testString.setRows(estimatedRows(testString));
-		    pack();
+	    pack();
 	}
     }
 
@@ -129,83 +130,83 @@ public class PatternMatcher extends JFrame {
 
     private final class CheckAction extends AbstractAction {
 
-        private CheckAction() {
-            super("Check match");
-        }
+	private CheckAction() {
+	    super("Check match");
+	}
 
-        public void actionPerformed(ActionEvent e) {
-			result.setLineWrap(true);
-            result.setText("");
-            try {
-                String patternText = pattern.getText();
-                prefs.put("patternText", patternText);
-                final List<String> testStringTexts = new ArrayList<String>();
-                for (int i = 0, n = testStrings.size(); i < n; i++) {
-                    final JTextArea t = testStrings.get(i);
-                    String testStringText = t.getText();
-                    if (testStringText.trim().isEmpty()) {
-                        prefs.put("testStringText" + i, "");
-                        continue;
-                    }
-                    testStringTexts.add(testStringText);
-                    prefs.put("testStringText" + i, testStringText);
-                    Pattern compiledPattern = Pattern.compile(patternText);
-                    Matcher matcher = compiledPattern.matcher(testStringText);
-					String resultLabel = "Field "
-							+ (i + 1)
-							+ ": "
-							+ testStringText.substring(0,
-									Math.min(testStringText.length(), 20))
-							+ (testStringText.length() >= 20 ? "..." : "");
-					result.append("--- matches() for " + resultLabel + " ---\n");
-                    if (!matcher.matches()) {
-                        result.append("string does not match regex\n");
-                    } else {
-                        appendMatchedGroups(matcher);
-                    }
-                    Matcher matcher2 = compiledPattern.matcher(testStringText);
-                    result.append("\n--- find() for " + resultLabel
-                            + " ---\n");
-                    boolean find = matcher2.find();
-                    if (!find) {
-                        result.append("no matches found\n");
-                    }
-                    int iteration = 1;
-                    do {
-                        if (find) {
-                            result.append("Iteration " + iteration++ + ":\n");
-                            appendMatchedGroups(matcher2);
-                            result.append("\n");
-                        }
-                    } while (find = matcher2.find());
-                    result.append("\n");
-                }
+	public void actionPerformed(ActionEvent e) {
+	    result.setLineWrap(true);
+	    result.setText("");
+	    try {
+		String patternText = pattern.getText();
+		prefs.put("patternText", patternText);
+		final List<String> testStringTexts = new ArrayList<String>();
+		for (int i = 0, n = testStrings.size(); i < n; i++) {
+		    final JTextArea t = testStrings.get(i);
+		    String testStringText = t.getText();
+		    if (testStringText.trim().isEmpty()) {
+			prefs.put("testStringText" + i, "");
+			continue;
+		    }
+		    testStringTexts.add(testStringText);
+		    prefs.put("testStringText" + i, testStringText);
+		    Pattern compiledPattern = Pattern.compile(patternText);
+		    Matcher matcher = compiledPattern.matcher(testStringText);
+		    String resultLabel = "Field "
+			    + (i + 1)
+			    + ": "
+			    + testStringText.substring(0,
+				    Math.min(testStringText.length(), 20))
+			    + (testStringText.length() >= 20 ? "..." : "");
+		    result.append("--- matches() for " + resultLabel + " ---\n");
+		    if (!matcher.matches()) {
+			result.append("string does not match regex\n");
+		    } else {
+			appendMatchedGroups(matcher);
+		    }
+		    Matcher matcher2 = compiledPattern.matcher(testStringText);
+		    result.append("\n--- find() for " + resultLabel + " ---\n");
+		    boolean find = matcher2.find();
+		    if (!find) {
+			result.append("no matches found\n");
+		    }
+		    int iteration = 1;
+		    do {
+			if (find) {
+			    result.append("Iteration " + iteration++ + ":\n");
+			    appendMatchedGroups(matcher2);
+			    result.append("\n");
+			}
+		    } while (find = matcher2.find());
+		    result.append("\n");
+		}
 		result.setRows(estimatedRows(result));
-            } catch (Exception ex) {
-                JTextArea textarea = new JTextArea(ex.getMessage());
-                textarea.setFont(new Font("Monospaced", Font.PLAIN, 16));
-                JOptionPane.showMessageDialog(PatternMatcher.this, textarea, ex.getClass().getName(), JOptionPane.ERROR_MESSAGE);
-            }
-        }
+	    } catch (Exception ex) {
+		JTextArea textarea = new JTextArea(ex.getMessage());
+		textarea.setFont(new Font("Monospaced", Font.PLAIN, 16));
+		JOptionPane.showMessageDialog(PatternMatcher.this, textarea, ex
+			.getClass().getName(), JOptionPane.ERROR_MESSAGE);
+	    }
+	}
 
-        private void appendMatchedGroups(Matcher matcher) {
-            result.append("Capturing group " + 0 + ": " + matcher.group()
-                    + "\n");
-            for (int i = 1, n = matcher.groupCount() + 1; i < n; i++) {
-                result.append("Capturing group " + i + ": " + matcher.group(i)
-                        + "\n");
-            }
-        }
+	private void appendMatchedGroups(Matcher matcher) {
+	    result.append("Capturing group " + 0 + ": " + matcher.group()
+		    + "\n");
+	    for (int i = 1, n = matcher.groupCount() + 1; i < n; i++) {
+		result.append("Capturing group " + i + ": " + matcher.group(i)
+			+ "\n");
+	    }
+	}
     }
 
     private PatternMatcher() {
-        super("Regex Matcher");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+	super("Regex Matcher");
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
 	configureTextFieldsAndResultArea();
 	configureNorthPanel();
 	configureUpperArea();
 	configureLowerArea();
-        configureTextFieldsForPatternTest();
+	configureTextFieldsForPatternTest();
     }
 
     private void configureTextFieldsAndResultArea() {
@@ -223,12 +224,19 @@ public class PatternMatcher extends JFrame {
     }
 
     private void configureUpperArea() {
-	textAreaContainer.setLayout(new GridBagLayout());
-	upper.add(new JScrollPane(textAreaContainer));
+	upper.setLayout(new GridBagLayout());
+	upper.add(labelTestString, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+		GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+		new Insets(2, 2, 2, 2), 0, 0));
+	upper.add(new JScrollPane(textAreaContainer), new GridBagConstraints(1,
+		0, 1, 1, 1.0, 1.0, GridBagConstraints.NORTHWEST,
+		GridBagConstraints.BOTH, new Insets(2, 2, 2, 2), 0, 0));
 	upperButtons.setLayout(new GridBagLayout());
 	addAnotherTestStringFieldButtonOnFirstRow();
 	addCheckButton();
-	upper.add(upperButtons, BorderLayout.EAST);
+	upper.add(upperButtons, new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+		GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+		new Insets(2, 2, 2, 2), 0, 0));
 	centerSplitArea.setLeftComponent(upper);
     }
 
@@ -240,23 +248,27 @@ public class PatternMatcher extends JFrame {
 
     private void configureTextFieldsForPatternTest() {
 	textAreaContainer.removeAll();
-	textAreaContainer.setLayout(new GridBagLayout()); // TODO
-        addTestStringsLabel();
+	textAreaContainer.setLayout(new GridBagLayout());
 	testStrings.clear();
 	for (String testStringText : testStringTexts()) {
-			final JTextArea testString = new JTextArea();
-			testString.setLineWrap(true);
-			testString.setText(testStringText);
+	    final JTextArea testString = new JTextArea();
+	    testString.setLineWrap(true);
+	    testString.setText(testStringText);
 	    testString.setColumns(80);
 	    testString.setRows(estimatedRows(testString));
-	    testString.getDocument().addDocumentListener(new TextFieldSizeAdapter(testString));
-            testStrings.add(testString);
+	    testString.getDocument().addDocumentListener(
+		    new TextFieldSizeAdapter(testString));
+	    testStrings.add(testString);
 	    textAreaContainer.add(new JScrollPane(testString),
 		    new GridBagConstraints(1, testStrings.size() - 1, 1, 1,
-			    1.0, 0.0, GridBagConstraints.WEST,
+			    0.0, 0.0, GridBagConstraints.NORTHWEST,
 			    GridBagConstraints.HORIZONTAL, new Insets(2, 2, 2,
 				    2), 0, 20));
 	}
+	textAreaContainer.add(new JPanel(), new GridBagConstraints(1,
+		testStrings.size(), 1, 1, 0.0, 1.0,
+		GridBagConstraints.NORTHWEST, GridBagConstraints.BOTH,
+		new Insets(2, 2, 2, 2), 0, 0));
 	upper.invalidate();
 	upper.revalidate();
 	upper.repaint();
@@ -275,11 +287,10 @@ public class PatternMatcher extends JFrame {
     }
 
     private void addCheckButton() {
-	upperButtons.add(
-                new JButton(new CheckAction()),
+	upperButtons.add(new JButton(new CheckAction()),
 		new GridBagConstraints(0, 1, 1, 1, 0.0, 0.0,
-                GridBagConstraints.EAST, GridBagConstraints.NONE,
-                new Insets(2, 2, 2, 2), 0, 0));
+			GridBagConstraints.EAST, GridBagConstraints.NONE,
+			new Insets(2, 2, 2, 2), 0, 0));
     }
 
     private int estimatedRows(JTextArea text) {
@@ -293,56 +304,38 @@ public class PatternMatcher extends JFrame {
     }
 
     private void addAnotherTestStringFieldButtonOnFirstRow() {
-	upperButtons.add(
-	        new JButton(new AddAnotherTestStringField()),
+	upperButtons.add(new JButton(new AddAnotherTestStringField()),
 		new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
-	        GridBagConstraints.WEST,
-	        GridBagConstraints.NONE,
-	        new Insets(2, 2, 2, 2), 0, 0));
-    }
-
-    private void addTestStringsLabel() {
-	textAreaContainer.add(
-                new JLabel("Teststring"),
- new GridBagConstraints(
-		0, 0, 1, 1, 0.0, 0.0,
-                GridBagConstraints.NORTHEAST,
-                GridBagConstraints.HORIZONTAL, new Insets(2, 2, 2, 2),
-                0, 0));
+			GridBagConstraints.WEST, GridBagConstraints.NONE,
+			new Insets(2, 2, 2, 2), 0, 0));
     }
 
     private void addPatternRow() {
-	northPanel.add(
-                new JLabel("Pattern"),
-                new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
-                GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
-                new Insets(2, 2, 2, 2), 0, 0));
-        pattern.setText(prefs.get("patternText", ""));
-	northPanel.add(
-                pattern,
-                new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0,
-                GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
-                new Insets(2, 2, 2, 2), 50, 0));
-	northPanel.add(
-                new JButton(new AddLevelToBackslashes()),
-                new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
-                GridBagConstraints.EAST, GridBagConstraints.NONE,
-                new Insets(2, 2, 2, 2), 0, 0));
-	northPanel.add(
-		new JButton(new RemoveLevelFromBackslashes()),
-                new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0,
-                GridBagConstraints.WEST, GridBagConstraints.NONE,
-                new Insets(2, 2, 2, 2), 0, 0));
+	northPanel.add(new JLabel("Pattern"), new GridBagConstraints(0, 0, 1,
+		1, 0.0, 0.0, GridBagConstraints.WEST,
+		GridBagConstraints.HORIZONTAL, new Insets(2, 2, 2, 2), 0, 0));
+	pattern.setText(prefs.get("patternText", ""));
+	northPanel.add(pattern, new GridBagConstraints(1, 0, 1, 1, 1.0, 0.0,
+		GridBagConstraints.WEST, GridBagConstraints.HORIZONTAL,
+		new Insets(2, 2, 2, 2), 50, 0));
+	northPanel.add(new JButton(new AddLevelToBackslashes()),
+		new GridBagConstraints(2, 0, 1, 1, 0.0, 0.0,
+			GridBagConstraints.EAST, GridBagConstraints.NONE,
+			new Insets(2, 2, 2, 2), 0, 0));
+	northPanel.add(new JButton(new RemoveLevelFromBackslashes()),
+		new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0,
+			GridBagConstraints.WEST, GridBagConstraints.NONE,
+			new Insets(2, 2, 2, 2), 0, 0));
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+	SwingUtilities.invokeLater(new Runnable() {
 
-            public void run() {
-                PatternMatcher patternMatcher = new PatternMatcher();
-				patternMatcher.setVisible(true);
-				patternMatcher.pack();
-            }
-        });
+	    public void run() {
+		PatternMatcher patternMatcher = new PatternMatcher();
+		patternMatcher.setVisible(true);
+		patternMatcher.pack();
+	    }
+	});
     }
 }

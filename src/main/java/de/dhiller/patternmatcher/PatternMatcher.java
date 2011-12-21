19f80c3dd4dc2d1a1a10693f1c3ff917eb49158c
Daniel Hiller <daniel.hiller.1972@googlemail.com>
2011-12-21 07:27:16 +0100

    Layout changes

--- src/main/java/de/dhiller/patternmatcher/PatternMatcher.java
+++ src/main/java/de/dhiller/patternmatcher/PatternMatcher.java
@@ -32,6 +32,7 @@
 package de.dhiller.patternmatcher;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
